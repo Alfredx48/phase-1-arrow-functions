9e007ca3e29e1f1f502164a643856cb0faa6ac3e
Alfred Shaheen <shaheenalfred48@gmail.com>
2023-03-24 16:25:06 -0700

    commit tree 65958bf6d09015435e06c969b71b70b77ef3ef3b

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide = (num1, num2) => num1 / num2;
+
+// divide(2000,100);
+
+// const square = num => num * num
+
+// square(1)
+
+// const add = (num1,num2) => num1 + num2;
+
+// add(1,2)
+
+// function multiply(a, b) {
+//   a * b
+// }
+// multiply(2,3)
